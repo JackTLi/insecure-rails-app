51b154756057315c66c07fc5f073913b3dc366c6
Jack Li <jack.li@shopify.com>
2017-10-12 11:59:13 -0400

    final

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    target_account = User.find_by(email: params[:session][:email])
-    if target_account && target_account.password == params[:session][:password]
-      session[:user_id] = target_account.id
-      redirect_to root_path, flash: { notice: "Hi #{target_account.first_name}" }
+    # target_account = User.find_by(email: params[:session][:email])
+    # if target_account && target_account.password == params[:session][:password]
+    #   session[:user_id] = target_account.id
+    #   redirect_to root_path, flash: { notice: "Hi #{target_account.first_name}" }
+    # else
+    #   redirect_to login_path, flash: { alert: "Invalid credentials" }
+    # end
+
+    results = ActiveRecord::Base.connection.execute("select * from users where email=\"#{params[:session][:email]}\" and password=\"#{params[:session][:password]}\" limit 1")
+    target_account = results.first
+
+    if target_account
+      session[:user_id] = target_account["id"]
+      redirect_to root_path, flash: { notice: "Hi #{target_account["first_name"]}" }
     else
       redirect_to login_path, flash: { alert: "Invalid credentials" }
     end
